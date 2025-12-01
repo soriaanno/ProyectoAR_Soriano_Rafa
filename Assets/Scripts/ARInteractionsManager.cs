ff02bbdb93b48e5b98c1bdd6b7690889b3cfdf60
soriaanno <soriano8822@gmail.com>
2025-12-01 19:22:21 +0100

    paso 5 finalizado

---
+++ Assets/Scripts/ARInteractionsManager.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+using UnityEngine.EventSystems;
+
+public class ARInteractionsManager : MonoBehaviour
+{
+    [SerializeField] private Camera aRCamera;
+    private ARRaycastManager aRRaycastManager;
+    private List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    private GameObject aRPointer;
+    private GameObject item3DModel;
+    private bool isInitialPosition;
+    private bool isOverUI;
+
+
+    public GameObject Item3DModel
+    {
+        set
+        {
+            item3DModel = value;
+            item3DModel.transform.position = aRPointer.transform.position;
+            item3DModel.transform.parent = aRPointer.transform;
+            isInitialPosition = true;
+        }
+    }
+
+    void Start()
+    {
+        aRPointer = transform.GetChild(0).gameObject;
+        aRRaycastManager = FindObjectOfType<ARRaycastManager>();
+        GameManager.instance.OnMainMenu += SetItemPosition;
+    }
+
+    void Update()
+    {
+        if (isInitialPosition)
+        {
+            Vector2 middlePointScreen = new Vector2(Screen.width / 2, Screen.height / 2);
+            aRRaycastManager.Raycast(middlePointScreen, hits, TrackableType.Planes);
+            if (hits.Count > 0)
+            {
+                transform.position = hits[0].pose.position;
+                transform.rotation = hits[0].pose.rotation;
+                aRPointer.SetActive(true);
+                isInitialPosition = false;
+            }
+        }
+        if (Input.touchCount > 0)
+        {
+            Touch touchOne = Input.GetTouch(0);
+            if (touchOne.phase == TouchPhase.Began)
+            {
+                var touchPosition = touchOne.position;
+                isOverUI = isTapOverUI(touchPosition);
+            }
+
+            if (touchOne.phase == TouchPhase.Moved)
+            {
+                if (aRRaycastManager.Raycast(touchOne.position, hits, TrackableType.Planes))
+                {
+                    Pose hitPose = hits[0].pose;
+                    if (!isOverUI)
+                    {
+                        transform.position = hitPose.position;
+                    }
+                }
+            }
+        }
+    }
+
+    private bool isTapOverUI(Vector2 touchPosition)
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current);
+        eventData.position = new Vector2(touchPosition.x, touchPosition.y);
+
+        List<RaycastResult> result = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, result);
+
+        return result.Count > 0;
+    }
+
+    private void SetItemPosition()
+    {
+        if (item3DModel != null)
+        {
+            item3DModel.transform.parent = null;
+            aRPointer.SetActive(false);
+            item3DModel = null;
+        }
+    }
+
+    public void DeleteItem()
+    {
+        Destroy(item3DModel);
+        aRPointer.SetActive(false);
+        GameManager.instance.MainMenu();
+    }
+}
+
--- Assets/Scripts/ItemButtonManager.cs
+++ Assets/Scripts/ItemButtonManager.cs
@@ -7,61 +7,36 @@ public class ItemButtonManager : MonoBehaviour
     private string itemDescription;
     private Sprite itemImage;
     private GameObject item3DModel;
+    private ARInteractionsManager interactionsManager;
 
-    // Propiedades completas con getter y setter
     public string ItemName
     {
-        get { return itemName; }
-        set { itemName = value; }
+        set
+        {
+            itemName = value;
+        }
     }
 
-    public string ItemDescription
-    {
-        get { return itemDescription; }
-        set { itemDescription = value; }
-    }
+    public string ItemDescription { set => itemDescription = value; }
+    public Sprite ItemImage { set => itemImage = value; }
+    public GameObject Item3DModel { set => item3DModel = value; }
 
-    public Sprite ItemImage
-    {
-        get { return itemImage; }
-        set { itemImage = value; }
-    }
-
-    public GameObject Item3DModel
-    {
-        get { return item3DModel; }
-        set { item3DModel = value; }
-    }
-
+    // Start is called before the first frame update
     void Start()
     {
-        // Comprueba que los elementos existen antes de asignar
-        if (transform.childCount >= 3)
-        {
-            var nameText = transform.GetChild(0).GetComponent<Text>();
-            if (nameText != null) nameText.text = itemName;
+        transform.GetChild(0).GetComponent<Text>().text = itemName;
+        transform.GetChild(1).GetComponent<RawImage>().texture = itemImage.texture;
+        transform.GetChild(2).GetComponent<Text>().text = itemDescription;
 
-            var imageRaw = transform.GetChild(1).GetComponent<RawImage>();
-            if (imageRaw != null && itemImage != null) imageRaw.texture = itemImage.texture;
-
-            var descText = transform.GetChild(2).GetComponent<Text>();
-            if (descText != null) descText.text = itemDescription;
-        }
-
-        // Añade listeners al botón
         var button = GetComponent<Button>();
-        if (button != null)
-        {
-            if (GameManager.instance != null)
-                button.onClick.AddListener(GameManager.instance.ARPosition);
+        button.onClick.AddListener(GameManager.instance.ARPosition);
+        button.onClick.AddListener(Create3DModel);
 
-            button.onClick.AddListener(Create3DModel);
-        }
+        interactionsManager = FindObjectOfType<ARInteractionsManager>();
     }
 
     private void Create3DModel()
     {
-        if (item3DModel != null)
-            Instantiate(item3DModel);
+        interactionsManager.Item3DModel = Instantiate(item3DModel);
     }
 }

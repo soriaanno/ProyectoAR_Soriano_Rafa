20f16c49f086f36d9e6d54f5f497808cfec4b663
rsc0013 <rsolcab@g.educaand.es>
2025-12-02 20:53:25 +0100

    Commit paso 6 realizado
    Paso 6 completado, build creada y probada en una ndoird, la camara no funciona correctamente

--- Assets/Scripts/ARInteractionsManager.cs
+++ Assets/Scripts/ARInteractionsManager.cs
@@ -13,9 +13,14 @@ public class ARInteractionsManager : MonoBehaviour
 
     private GameObject aRPointer;
     private GameObject item3DModel;
+    private GameObject itemSelected;
+
     private bool isInitialPosition;
     private bool isOverUI;
+    private bool isOver3DModel;
 
+    private Vector2 initialTouchPos;
+    
 
     public GameObject Item3DModel
     {
@@ -56,6 +61,7 @@ public class ARInteractionsManager : MonoBehaviour
             {
                 var touchPosition = touchOne.position;
                 isOverUI = isTapOverUI(touchPosition);
+                isOver3DModel = isTapOver3DModel(touchPosition);
             }
 
             if (touchOne.phase == TouchPhase.Moved)
@@ -63,14 +69,58 @@ public class ARInteractionsManager : MonoBehaviour
                 if (aRRaycastManager.Raycast(touchOne.position, hits, TrackableType.Planes))
                 {
                     Pose hitPose = hits[0].pose;
-                    if (!isOverUI)
+                    if (!isOverUI && isOver3DModel) //if(!isOverUI && isOver3DModel && item3DModel.name == itemSelected.name)
                     {
                         transform.position = hitPose.position;
                     }
                 }
             }
+
+            if (Input.touchCount == 2)
+            {
+                Touch touchTwo = Input.GetTouch(1);
+                if (touchOne.phase == TouchPhase.Began || touchTwo.phase == TouchPhase.Began)
+                {
+                    initialTouchPos = touchTwo.position - touchOne.position;
+                }
+
+                if (touchOne.phase == TouchPhase.Moved || touchTwo.phase == TouchPhase.Moved)
+                {
+                    Vector2 currentTouchPos = touchTwo.position - touchOne.position;
+                    float angle = Vector2.SignedAngle(initialTouchPos, currentTouchPos);
+                    item3DModel.transform.rotation = Quaternion.Euler(0, item3DModel.transform.eulerAngles.y - angle, 0);
+                    initialTouchPos = currentTouchPos;
+                }
+            }
+
+            if (isOver3DModel && item3DModel == null && !isOverUI)
+            {
+                GameManager.instance.ARPosition();
+                item3DModel = itemSelected;
+                itemSelected = null;
+                aRPointer.SetActive(true);
+                transform.position = item3DModel.transform.position;
+                item3DModel.transform.parent = aRPointer.transform;
+            }
+
         }
     }
+
+    private bool isTapOver3DModel(Vector2 touchPosition)
+    {
+        Ray ray = aRCamera.ScreenPointToRay(touchPosition);
+        if (Physics.Raycast(ray, out RaycastHit hit3DModel))
+        {
+            if (hit3DModel.collider.CompareTag("Item"))
+            {
+                itemSelected = hit3DModel.transform.gameObject;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
 
     private bool isTapOverUI(Vector2 touchPosition)
     {
